556db73716ea01a259b0444ae9cad395e9658926
JonathanGRiego02 <jonathangrclases@gmail.com>
2024-10-03 11:51:31 +0100

    Construccion controller

---
+++ MisAmigos/src/main/java/org/dad/ModificarController.java
@@ -0,0 +1,51 @@
+package org.dad;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModificarController implements Initializable {
+
+    @FXML
+    private BorderPane root = new BorderPane();
+
+    @FXML
+    private Button enemyButton;
+
+    @FXML
+    private Button friendButton;
+
+
+    ModificarController() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/ModificarView.fxml"));
+            loader.setController(this);
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    void onenemyAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onfriendAction(ActionEvent event) {
+
+    }
+
+}
